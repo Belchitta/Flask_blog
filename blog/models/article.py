b24563728b8b8e6047cefcd4606918d42c418392
Belchitta <misschernenok@gmail.com>
2023-04-09 11:20:09 +0300

    Author, article models db

--- blog/models/article.py
+++ blog/models/article.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, func
+from datetime import datetime
+from sqlalchemy.orm import relationship
 from blog.extensions import db
 
 
 class Article(db.Model):
-    id = Column(Integer, primary_key=True)
-    title = Column(String(300), unique=True, nullable=False)
-    text = Column(Text, default='Empty')
-    date = Column(DateTime(timezone=True), default=func.now())
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(300), unique=True, nullable=False)
+    text = db.Column(db.Text, default='Empty')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def __repr__(self):
-        return f"Article #{self.id} {self.title!r} {self.date!r}>"
+    author = relationship('Author', back_populates='articles')
