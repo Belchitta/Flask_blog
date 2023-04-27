ad230350c7146a52bcf419cfa1bb01009957d8cb
Belchitta <misschernenok@gmail.com>
2023-04-27 23:35:15 +0300

    # Conflicts: #	blog/__pycache__/app.cpython-310.pyc #	blog/__pycache__/extensions.cpython-310.pyc #	blog/app.py #	blog/models/__pycache__/user.cpython-310.pyc #	blog/models/user.py #	blog/views/__pycache__/auth.cpython-310.pyc #	blog/views/auth.py #	requirements.txt

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,13 +1,22 @@
 from flask_login import UserMixin
-from sqlalchemy import Column, Integer, String, Boolean
-from blog.models.database import db
+from blog.extensions import db
 
 
 class User(db.Model, UserMixin):
-    id = Column(Integer, primary_key=True)
-    username = Column(String(80), unique=True, nullable=False)
-    is_staff = Column(Boolean, nullable=False, default=False)
-    email = Column(String(255), nullable=False, default="", server_default="")
+    __tablename__ = 'users'
 
-    def __repr__(self):
-        return f"User #{self.id} {self.username!r}>"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    is_staff = db.Column(db.Boolean, nullable=False, default=False)
+    email = db.Column(db.String(255), nullable=False, default="", server_default="")
+    password = db.Column(db.String(255))
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+
+    def __init__(self, email, username, is_staff, first_name, last_name, password):
+        self.email = email
+        self.username = username
+        self.is_staff = is_staff
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
